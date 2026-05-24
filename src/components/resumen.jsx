f2fb11fa9a64fe1e5d8c30941073ee10378e976f
Alejandralx22 <alejandralx22@gmail.com>
2026-05-24 19:20:09 -0400

    componente resumen

---
+++ src/components/resumen.jsx
@@ -0,0 +1,15 @@
+function Resumen() {
+  return (
+    <div className="bg-white rounded-lg shadow p-8">
+      <h2 className="text-2xl font-bold mb-4">
+        Resumen Ejecutivo
+      </h2>
+
+      <p className="text-slate-700 leading-relaxed">
+        El grupo de ransomware BlackCat logró acceder a los sistemas de Change Healthcare utilizando credenciales robadas de un servicio de acceso remoto que no contaba con autenticación multifactor (MFA).
+      </p>
+    </div>
+  )
+}
+
+export default Resumen
