4c22b5f531d32b24c1153d650104ccb4a8e19673
Alejandralx22 <alejandralx22@gmail.com>
2026-05-25 12:54:49 -0400

    resumen react

--- src/components/resumen.jsx
+++ src/components/resumen.jsx
@@ -8,7 +8,6 @@ export default function Resumen() {
       subtitle="Caso blackcat"
       file="01_resumen_lopeli.md"
       Icon={FileText}
-      Icon={(props) => <FileText {...props} className="text-red-500" />}
     />
   )
 }
