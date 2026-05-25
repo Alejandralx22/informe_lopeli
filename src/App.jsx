a79b310eb702c8bbf94602ed9d880d2fd7a80ec4
Alejandralx22 <alejandralx22@gmail.com>
2026-05-25 14:12:54 -0400

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Resumen from "./components/Resumen"
 import Marco from "./components/marco"
+import Delitos from "./components/delitos"
+import Comparacion from "./components/comparacion"
 
 function App() {
   return (
